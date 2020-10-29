02f1652a64ec9ae1135bf5e38d0ec2e5c874ac99
Vicente Guevara Ayala <31393551+Visistema@users.noreply.github.com>
2020-10-29 15:41:31 -0400

    Create ReachabilityTests.java

---
+++ src/test/java/org/masterCard/cityhop/ReachabilityTests.java
@@ -0,0 +1,58 @@
+package org.masterCard.cityhop;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ReachabilityTests {
+
+	private Roadmap roadmap;
+
+	public ReachabilityTests() {
+		this.roadmap = new Roadmap();
+		preload(roadmap);
+	}
+
+	@Test
+	public void targetIsSource() {
+		ReachabilityCheck task = new ReachabilityCheck(roadmap, "A1", "A1");
+		assertEquals(task.exec(), ReachabilityCheck.Result.REACHABLE);		
+	}
+	
+	
+	@Test
+	public void targetIsOneHop() {
+		ReachabilityCheck task = new ReachabilityCheck(roadmap, "A1", "A2");
+		assertEquals(task.exec(), ReachabilityCheck.Result.REACHABLE);		
+	}
+	
+	@Test
+	public void targetIsMultipleHops() {
+		ReachabilityCheck task = new ReachabilityCheck(roadmap, "A1", "A3");
+		assertEquals(task.exec(), ReachabilityCheck.Result.REACHABLE);
+	}
+	
+	@Test
+	public void targetIsUnreachable() {
+		ReachabilityCheck task = new ReachabilityCheck(roadmap, "A1", "B1");
+		assertEquals(task.exec(), ReachabilityCheck.Result.UNREACHABLE);		
+	}
+	
+	@Test
+	public void sourceIsUnknown() {
+		ReachabilityCheck task = new ReachabilityCheck(roadmap, "C1", "A1");
+		assertEquals(task.exec(), ReachabilityCheck.Result.ORIGIN_UNKNOWN);		
+	}
+	
+	@Test
+	public void targetIsUnknown() {
+		ReachabilityCheck task = new ReachabilityCheck(roadmap, "A1", "C1");
+		assertEquals(task.exec(), ReachabilityCheck.Result.DESINATION_UNKNOWN);		
+	}
+
+	private static void preload(Roadmap roadmap) {
+		roadmap.addNeighbors("A1",  "A2");
+		roadmap.addNeighbors("A2",  "A3");
+		roadmap.addNeighbors("B1",  "B2");
+	}
+}
